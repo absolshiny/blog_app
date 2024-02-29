fd10fb87fe570257ee2cff5139224aa090f6cccf
DSegura <absol.1994@gmail.com>
2024-02-28 20:42:12 -0600

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -9,3 +9,12 @@ CREATE TABLE messages (
     contact_id INTEGER REFERENCES contacts(id) ON DELETE CASCADE,
     message TEXT NOT NULL
 );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    user_email VARCHAR(20),
+    password VARCHAR(16)
+);
+
+INSERT INTO users (user_email, password)
+VALUES ('david@example.com', 'just_password');
