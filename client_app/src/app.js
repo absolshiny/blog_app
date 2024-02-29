5e95413a762cf3268394c52202ef7f14567fb8c9
DSegura <absol.1994@gmail.com>
2024-02-29 00:50:45 -0600

    Create app.js

---
+++ client_app/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './App.css'; // Import optional application-wide styles
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      {/* Add other components here */}
+    </div>
+  );
+}
+
+export default App;
