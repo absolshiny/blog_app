e985d2f2a74dafa2e9d0ea5e339e029cd353695b
DSegura <absol.1994@gmail.com>
2024-02-28 18:18:58 -0600

    adding db

--- server_app/server/controllers/controller.js
+++ server_app/server/controllers/controller.js
@@ -1,14 +1,43 @@
+const { User, Message } = require('../models/models');
 
 const getallnews = async (req, res) => {
-    res.json('news');
+    res.send(JSON.stringify(req.data));
 };
 
 const add_contact = async (req, res) => {
-    res.json('contac added');
+    try{
+        const user_obj = {
+            email : req.body.email,
+            cellphone : req.body.cellphone
+        };
+        console.log(user_obj);
+        var UserFrDb = await User.getByEmail(user_obj.email);
+        if (!UserFrDb) {
+            UserFrDb = await User.addUser(user_obj);
+            console.log(`user ${UserFrDb.email} added`);
+        } else {
+            console.log(`user ${UserFrDb.email} found`);
+        }
+        console.log(UserFrDb);
+        const messageObj = {
+            contact_id: UserFrDb.id,
+            message: req.body.message
+        };
+        const message = await Message.insertMessage(messageObj);
+
+        if (message) {
+            res.send('message added');
+        }
+    }   
+    catch (error){
+        console.error(error);
+        // Handle other errors appropriately
+        throw error; // Re-throw for potential error handling in caller
+    }
 };
 
 const login = async (req, res) => {
-    res.json('logged');
+    res.send('logged');
 };
 
 module.exports = {
