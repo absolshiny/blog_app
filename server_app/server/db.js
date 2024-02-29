57a6a66fb7735c848c4a199574314def1674c4bf
DSegura <absol.1994@gmail.com>
2024-02-28 18:19:01 -0600

    Create db.js

---
+++ server_app/server/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'david',
+  password: 'david123',
+  host: '172.17.0.2',
+  port: '5432',
+  database: 'Bloginfo',
+});
+
+module.exports = pool;
