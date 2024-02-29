e47338ec27bf0f72289c05bc043cbe1df4e753b3
DSegura <absol.1994@gmail.com>
2024-02-28 20:42:21 -0600

    Update models.js

--- server_app/server/models/models.js
+++ server_app/server/models/models.js
@@ -62,9 +62,9 @@ const Message = {
       const query = `INSERT INTO ${this.tableName} (contact_id, message) VALUES ($1, $2) RETURNING *`;
       const result = await client.query(query, [message.contact_id, message.message]);
       const newMessage = result.rows[0];
-      return newMessage;
+      return ({code: 201, message: newMessage});
     } catch (error) {
-      throw error;
+      return error;
     } finally {
       client.release(); // Release the connection back to the pool
     }
@@ -87,4 +87,36 @@ const Message = {
 
 };
 
-module.exports = { User, Message };
+// Message model (schema)
+const adminBlog = {
+  tableName: 'users',
+  fields: ['id', 'user_email', 'password'],
+
+  // Function to insert a new message
+  async ValidateUser(creds) {
+    const client = await pool.connect();
+    try {
+      const query = `SELECT * FROM ${this.tableName} WHERE user_email = $1`;
+      const result = await client.query(query, [creds.user_email]);
+      const user = result.rows[0];
+      if (!user) {
+        throw new Error(`User with email ${creds.user_email} not found`);
+      }
+      if (creds.password !== user.password) {
+        throw new Error('Incorrect password');
+      }
+      return {code: 200, message: "success"};
+    } catch (error) {
+      console.error(error);
+      if (error.message.includes('not found')) {
+        return({code: 404, message: error.message});
+      } else {
+        return({code: 401, message: error.message});
+      }
+    } finally {
+      client.release();
+    }
+  }
+};
+
+module.exports = { User, Message, adminBlog };
