f260a3d737d4e87900f1ea078d1a1287f3539df0
DSegura <absol.1994@gmail.com>
2024-03-01 01:09:25 -0600

    Update index.js

--- server_app/server/index.js
+++ server_app/server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router_app = require('./routes'); 
+const cors = require('cors');
+const pool = require('./db');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const pool = require('./db');
+app.use(cors());
 
 app.use('/', router_app);
 
