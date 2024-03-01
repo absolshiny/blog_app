89f7ab4ab8ee0782bfe0d25baf35df3115ab2601
DSegura <absol.1994@gmail.com>
2024-02-29 22:05:03 -0600

    Update newsBox.js

--- client_app/src/components/newsBox.js
+++ client_app/src/components/newsBox.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/newsBox.css';
 
-function NewsBox({ image, content, isLeft }) {
-    const truncatedContentlength = Math.min(content.length, 100);
-    const truncatedContent = content.slice(0, truncatedContentlength);
+function NewsBox({ item, onItemClick, isLeft }) {
+    const truncatedContentlength = Math.min(item.content.length, 100);
+    const truncatedContent = item.content.slice(0, truncatedContentlength);
 
     return (
-        <div className={`news-box ${isLeft ? 'left' : 'right'}`}>
-            <img src={image} alt="Box Image" />
+        <div className={`news-box ${isLeft ? 'left' : 'right'}`} onClick= {() => onItemClick(item)}>
+            <img src={item.image} alt="Box Image" />
             <div className="news-content">
                 <p>{truncatedContent}</p>
             </div>
